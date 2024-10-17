c98a8bb5309db2f834d6879b13ab04a4a46eaef2
“Ihan0316” <aijnjj12@gmail.com>
2024-10-18 02:24:41 +0900

    edit

--- test_homepage/assets/js/script.js
+++ test_homepage/assets/js/script.js
@@ -1,4 +1,3 @@
-// 배경 이미지 설정 함수
 function setBackgroundImage() {
     const body = document.body;
     const page = window.location.pathname.split("/").pop();
@@ -8,29 +7,28 @@ function setBackgroundImage() {
         'detail.html': "url('assets/img/bg2.jpg')",
         'sign_in.html': "url('assets/img/bg3.jpg')",
         'sign_up.html': "url('assets/img/bg4.jpg')",
-        // 추가적인 페이지에 대한 배경 이미지 설정
         'default': "url('assets/img/default_bg.jpg')"
     };
 
     body.style.backgroundImage = backgrounds[page] || backgrounds['default'];
 }
 
-// 링크에 마우스 오버 효과 추가 함수
-function addHoverEffect(links) {
+function addHoverEffect(links, cards) {
     links.forEach(link => {
         link.addEventListener('mouseover', () => {
-            link.style.transform = 'translateY(-5px)'; // 위로 이동
+            link.style.transform = 'translateY(-5px)';
         });
 
         link.addEventListener('mouseout', () => {
-            link.style.transform = 'translateY(0)'; // 원래 위치로 복귀
+            link.style.transform = 'translateY(0)';
         });
     });
 }
 
-// DOMContentLoaded 이벤트 리스너
 document.addEventListener("DOMContentLoaded", () => {
-    setBackgroundImage(); // 배경 이미지 설정
+    setBackgroundImage();
     const links = document.querySelectorAll('.navbar a');
-    addHoverEffect(links); // 링크에 마우스 오버 효과 추가
+    addHoverEffect(links);
+    const cards = document.querySelectorAll('.card');
+    addHoverEffect(cards);
 });
